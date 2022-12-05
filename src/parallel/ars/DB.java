db3299fde1f98907ed85bea71b7caa906bcfc4f3
Mohamed Ahraf <mohamed7elprans@yahoo.com>
2022-12-05 16:52:52 +0200

    Init project and database and create users table

---
+++ src/parallel/ars/DB.java
@@ -0,0 +1,66 @@
+package parallel.ars;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class DB {
+    public static List<Dictionary> UsersQuery(String query) {
+        // MySql Database Connection      
+        try{
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_fcih-db?characterEncoding=UTF-8",
+                    "freedb_new-user","yeFnSh&NdEEM3R3");
+            // Success Connection
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            List<Dictionary> results = new LinkedList<Dictionary>();
+            while(rs.next()) {
+                Dictionary result = new Hashtable();
+                result.put("firstName", rs.getString("firstName"));
+                result.put("lasttName", rs.getString("lastName"));
+                result.put("birthDate", rs.getString("birthDate"));
+                result.put("phoneNumber", rs.getString("phoneNumber"));
+                result.put("email", rs.getString("email"));
+                result.put("password", rs.getString("password"));
+                result.put("nationalID", rs.getString("nationalID"));
+                result.put("country", rs.getString("country"));
+                results.add(result);
+            }
+            connection.close();
+            return(results);
+            // Close the connection
+        }catch( Exception e ){
+            // Error in database
+            System.out.println("Error : "+ e);
+            JOptionPane.showMessageDialog(null ,"Error in Database Please check your connection!");
+            return(null);
+        }
+    } 
+    
+    public static boolean UpdateQuery(String query) {
+        // MySql Database Connection      
+        try{
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_fcih-db?characterEncoding=UTF-8",
+                    "freedb_new-user","yeFnSh&NdEEM3R3");
+            // Success Connection
+            Statement st = connection.createStatement();
+            st.executeUpdate(query);
+            connection.close();
+            System.out.println("Table updated successfully");
+            return(true);
+            // Close the connection
+        }catch( Exception e ){
+            // Error in database
+            System.out.println("Error : "+ e);
+            return(false);
+        }
+    } 
+}
---
+++ src/parallel/ars/ParallelARS.java
@@ -0,0 +1,25 @@
+package parallel.ars;
+
+import java.util.Dictionary;
+import java.util.List;
+
+public class ParallelARS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Keep it so we can create another database locally
+        // DB.UpdateQuery("Create Table users(id INT(64) NOT NULL AUTO_INCREMENT , firstName VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, birthDate VARCHAR(50) NOT NULL, phoneNumber VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, nationalID VARCHAR(50) NOT NULL, country VARCHAR(50) NOT NULL, PRIMARY KEY (ID))");
+        //DB.UpdateQuery("INSERT INTO users (firstName, lastName, birthDate, phoneNumber, email, password, nationalID, country )" +
+                   // "VALUES ('Mohamed', 'Ashraf', '2022/12/5', '+201000000000', 'mohamed@fcih.com', '123456789', '0123456789012', 'Egypt' );");
+        
+        // Example on how to get any data from database
+        List<Dictionary> users = DB.UsersQuery("Select * from users");
+        System.out.println(users.get(0).get("firstName"));
+        
+        new Login().setVisible(true);
+    }
+    
+}
